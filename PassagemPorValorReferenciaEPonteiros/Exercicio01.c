5794d555b74c6aaed301cf74a3ecac9ccd906c8d
EverttonFernandes <evertonoliveirafernandes@outlook.com>
2024-10-31 03:51:44 -0300

    Exercício 1 sobre passagem por referencia resolvido

---
+++ PassagemPorValorReferenciaEPonteiros/Exercicio01.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+/*
+Escreva uma função que receba um vetor de inteiros com tamanho 4 e um valor a ser somado. 
+A função deve somar esse valor a cada elemento do vetor, modificando o vetor original 
+(somando o valor ao número na posição 0, depois na posição 1, e assim por diante).
+Requisitos:
+O vetor original deve ser modificado diretamente pela função.
+A função deve somar o valor fornecido a cada elemento do vetor, modificando os elementos do vetor original.
+O programa principal deve exibir os valores do vetor original antes de chamar a função e após a função, para mostrar as alterações.
+Dicas:
+Ao passar um vetor para uma função em C, estamos implicitamente passando o vetor por referência 
+(não é necessário o uso explícito de ponteiros ou * no parâmetro da função). 
+Isso significa que o vetor original será modificado dentro da função que não precisa retornar um ponteiro, podendo ser ela uma função void.
+Não há passagem por valor direta de vetores em C. A função irá operar diretamente sobre o vetor original, modificando-o.
+*/
+void somarValorAoVetor(int vetor[], int valorASerSomado);
+void mostrarVetorNaTela(int vetor[]);
+
+int main() {
+    int vetor[4] = {1, 2, 3, 4};
+    int valorASerSomado;
+
+    printf("Vetor original: \n");
+    mostrarVetorNaTela(vetor);
+
+    printf("\nDigite o valor a ser somado aos elementos do vetor: ");
+    scanf("%d", &valorASerSomado);
+
+    somarValorAoVetor(vetor, valorASerSomado);
+
+    printf("\nVetor modificado: \n");
+    mostrarVetorNaTela(vetor);
+    return 0;
+}
+
+
+void somarValorAoVetor(int vetor[], int valorASerSomado) {
+    for (int indice = 0; indice < 4; indice++) {
+        vetor[indice] += valorASerSomado;
+    }
+}
+
+void mostrarVetorNaTela(int vetor[]){
+    for (int indice = 0; indice < 4; indice++) {
+        printf("%d ", vetor[indice]);
+    }
+    printf("\n");
+}
