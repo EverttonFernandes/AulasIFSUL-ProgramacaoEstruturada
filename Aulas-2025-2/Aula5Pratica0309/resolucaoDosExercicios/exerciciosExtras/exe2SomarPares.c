1b2e22e28a8f3b5e5a6603e1d5ee8ee94dd50846
EverttonFernandes <evertonoliveirafernandes@outlook.com>
2025-09-08 18:17:37 -0300

    Resolução dos dois últimos exercícios extras

--- Aulas-2025-2/Aula5Pratica0309/resolucaoDosExercicios/exerciciosExtras/exe2SomarPares.c
+++ Aulas-2025-2/Aula5Pratica0309/resolucaoDosExercicios/exerciciosExtras/exe2SomarPares.c
@@ -9,3 +9,34 @@ A função deve retornar a soma encontrada.
 Na main, leia o vetor, chame a função e mostre a soma dos números pares.
 
 */
+
+int somarPares(int *vetor, int tamanho);
+
+int main(){
+    int tamanho = 4;
+    int vetor[4];
+    
+    printf("Informe 4 numeros:\n");
+    for (int i = 0; i < tamanho; i++){
+        scanf("%d", &vetor[i]);
+    }
+    
+    int somaDosPares = somarPares(vetor, tamanho);
+
+    printf("A soma dos numeros pares foi %d\n", somaDosPares); // mostra na tela a soma dos numeros pares encontrados
+    
+    return 0;
+}
+
+int somarPares(int *vetor, int tamanho){
+    int somaDosPares = 0;
+
+    for (int i= 0; i < tamanho; i++){
+        int atualNumeroObtidoNaPosicao = *(vetor + i); // desreferencia o ponteiro para pegar o valor do vetor na posição atual(evitando tem que ficar usando * pra tudo - Assim podemos usar ponteiro uma única vez dentro do loop ;)
+        if (atualNumeroObtidoNaPosicao % 2 == 0){
+            somaDosPares += atualNumeroObtidoNaPosicao;  // se o atual numero for par, soma ele ao total
+        }
+    }
+    
+    return somaDosPares; // retorna a soma total dos numeros pares encontrados
+}
--- Aulas-2025-2/Aula5Pratica0309/resolucaoDosExercicios/exerciciosExtras/exe3MaiorElemento.c
+++ Aulas-2025-2/Aula5Pratica0309/resolucaoDosExercicios/exerciciosExtras/exe3MaiorElemento.c
@@ -9,3 +9,36 @@ A função deve retornar esse maior valor.
 Na main, leia os valores do vetor, chame a função e mostre qual foi o maior número encontrado.
 
 */
+
+int maiorElemento(int *vetor, int tamanho);
+
+int main(){
+    int tamanho = 4;
+    int vetor[4];
+    
+    printf("Informe 4 numeros:\n");
+    for (int indice = 0; indice < tamanho; indice++){
+        scanf("%d", &vetor[indice]);
+    }
+    
+    int maiorNumero = maiorElemento(vetor, tamanho);
+
+    printf("O maior numero encontrado foi %d\n", maiorNumero);
+    
+    return 0;
+}
+
+int maiorElemento(int *vetor, int tamanho){
+    int maiorNumeroEncontrado = 0; // inicializa com zero, pois qualquer numero do vetor será maior que ele
+
+    for (int indice = 0; indice < tamanho; indice++){
+        
+        int numeroAtualDentroDoVetor = *(vetor + indice); // desreferencia o ponteiro para pegar o valor do vetor na posição atual(evitando tem que ficar usando * pra tudo ;)
+        if (numeroAtualDentroDoVetor > maiorNumeroEncontrado){
+            maiorNumeroEncontrado = numeroAtualDentroDoVetor;  
+        }
+        
+    }
+    
+    return maiorNumeroEncontrado;
+}
